002574b9efec2412981154d7f5fa0ba107c4f7fe
tunasdj4 <tunasdj4@gmail.com>
2025-10-26 22:17:36 -0600

    PlayingCat

---
+++ Proyectos-java/src/PlayingCat.java
@@ -0,0 +1,20 @@
+public class PlayingCat {
+    public static void main(String[] args) {
+        System.out.println(isCatPlaying(true,10));
+        System.out.println(isCatPlaying(false,36));
+        System.out.println(isCatPlaying(false,35));
+
+
+
+    }
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+
+        if(!summer && temperature >= 25 && temperature <= 35){
+            return true;
+        }
+        if(summer && temperature >= 25 && temperature <= 45){
+            return true;
+        }
+        return false;
+    }
+}
