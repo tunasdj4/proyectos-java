5b8242be547d77ee05574d62f2133b2c25bc1c74
tunasdj4 <tunasdj4@gmail.com>
2025-11-07 17:29:58 -0600

    Create ChallengeEnchanced.java

---
+++ Proyectos-java/src/ChallengeEnchanced.java
@@ -0,0 +1,59 @@
+public class ChallengeEnchanced {
+
+    public static void main(String[] args) {
+        printDayOfWeek(0);
+        printDayOfWeek(1);
+        printDayOfWeek(2);
+        printDayOfWeek(3);
+        printDayOfWeek(4);
+        printDayOfWeek(5);
+        printDayOfWeek(6);
+        printDayOfWeek(7);
+        System.out.println("");
+        printWeekDay(0);
+        printWeekDay(1);
+        printWeekDay(2);
+        printWeekDay(3);
+        printWeekDay(4);
+        printWeekDay(5);
+        printWeekDay(6);
+        printWeekDay(7);
+        }
+    public static void printDayOfWeek(int day) {
+        String dayOfWeek = switch (day) {
+            case 0 -> {
+                yield "Sunday";
+            }
+            case 1 -> "Monday";
+            case 2 -> "Tuesday";
+            case 3 -> "Wednesday";
+            case 4 -> "Thursday";
+            case 5 -> "Friday";
+            case 6 -> "Saturday";
+            default -> "Invalid day";
+        };
+        System.out.println(day + " stands for " + dayOfWeek);
+    }
+
+    public static void printWeekDay(int day){
+
+        if (day == 0) {
+            System.out.println("Sunday");
+        } else if (day == 1) {
+            System.out.println("Monday");
+        } else if (day == 2) {
+            System.out.println("Tuesday");
+        }  else if (day == 3) {
+            System.out.println("Wednesday");
+        } else if (day == 4) {
+            System.out.println("Thursday");
+        } else if (day == 5) {
+            System.out.println("Friday");
+        } else if (day == 6) {
+            System.out.println("Saturday");
+        }else {
+            System.out.println("Invalid day");
+        }
+    }
+
+}
