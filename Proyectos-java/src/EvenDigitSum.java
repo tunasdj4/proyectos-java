7a5b63d203b0bbb3223b01a9df751e36fd9fe4d4
tunasdj4 <tunasdj4@gmail.com>
2026-01-04 11:18:45 -0600

    Create EvenDigitSum

---
+++ Proyectos-java/src/EvenDigitSum.java
@@ -0,0 +1,25 @@
+public class EvenDigitSum {
+    public static void main(String[] args) {
+        System.out.println(getEvenDigitSum(123456789));
+
+    }
+
+    public static int getEvenDigitSum(int number) {
+        if (number < 0) {
+            return -1;
+        }
+        int sum = 0;
+        while (number > 0) {
+            int lastDigit = number % 10;
+            if (lastDigit % 2 == 0 ) {
+                sum += lastDigit;
+            }
+            number /= 10;
+        }
+        return sum;
+    }
+
+
+
+
+}
