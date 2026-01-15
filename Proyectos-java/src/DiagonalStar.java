316651daf580a0511f698bfbcc3501bffc59cdad
tunasdj4 <tunasdj4@gmail.com>
2026-01-14 21:23:53 -0600

    Create DiagonalStar

---
+++ Proyectos-java/src/DiagonalStar.java
@@ -0,0 +1,24 @@
+public class DiagonalStar {
+    public static void main(String[] args) {
+
+       printSquareStar(8);
+
+    }
+
+    public static void printSquareStar(int number){
+        if(number < 5){
+            System.out.println("Invalid Value");
+            return;
+        }
+        for(int row = 0; row < number; row++){
+            for(int col = 0; col < number; col++){
+                if((row == 0) || (row == (number-1)) || (col == 0) || (col == (number-1)) || (row == col)  || (col == (number-row-1))){
+                    System.out.print("*");
+                }else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
